3c27270961f231f8afb7fa1bfdfd2047234f6561
Josue Plaza <plazajosue2@gmail.com>
2018-07-05 12:04:11 -0500

    updated dodgeball code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = "Josue";
+  active = true;
+  totalTeams = 4;
+  teams = [
+    {
+      teamName: 'Chupacabras',
+      players: ['Michael', 'Daira', 'Tiny', 'Kenny', 'Johnny'],
+      teamHome: 'Mission'
+    },
+    {
+      teamName: 'Wrench Dodgers',
+      players: ['Cesare', 'Erick', 'Victor', 'Jess', 'Jeremy'],
+      teamHome: 'New York'
+    },
+    {
+      teamName: 'Los Tamales',
+      players: ['Jorge', 'Jacqueline', 'Steven', 'Hiram', 'Luis'],
+      teamHome: 'Los Angeles'
+    },
+    {
+      teamName: 'Weeaboos',
+      players: ['Ernesto', 'Joshua', 'Emmanuel', 'Tony', 'Roxanne'],
+      teamHome: 'Tokyo'
+    }
+  ];
 }
